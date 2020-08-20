b98b5c344314ea8b493e689b10ae380367e380d4
k-saunagi <k5.daha55@gmail.com>
2020-08-20 18:01:03 +0900

    [add]primeを使わない記述

--- problem3.rb
+++ problem3.rb
@@ -6,3 +6,11 @@ require 'prime'
 
 p Prime.prime_division(600851475143).map{ |p| p[0] }.last
 
+# ======重くて動かない==========
+
+# n = 600851475143
+# array = (1..n).select do |i|
+#   n % i == 0 && (2..i-1).select{ |num| i % num == 0 }.empty?
+# end
+
+# p array.last
