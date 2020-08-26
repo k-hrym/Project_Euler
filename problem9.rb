df1895f92ce715fdaaa182b22398521f14cf8b1a
k-saunagi <k5.daha55@gmail.com>
2020-08-26 12:28:08 +0900

    [fix]simpler and faster

--- problem9.rb
+++ problem9.rb
@@ -5,30 +5,19 @@
 # a + b + c = 1000 となるピタゴラスの三つ組が一つだけ存在する.
 # これらの積 abc を計算しなさい.
 
-# ピタゴラスの定理を満たす平方数の組み合わせ配列を作る
+# ピタゴラスの定理を満たす平方数の組み合わせ配列から求める
 def pythagorean(nat_num)
   square_nums = [*1..nat_num].map{|int| int**2}
   i = 0
-  pytha_array = []
   square_nums.each do |num|
     square_nums[i..-1].each do |n|
-      if square_nums.include?(num + n)
-        pytha_array << [num, n, num + n]
+      if square_nums.include?(num + n) && \
+        [num, n, num + n].map{|square| Math.sqrt(square)}.sum == nat_num
+        return [num, n, num + n].map{|square| Math.sqrt(square)}
       end
     end
     i += 1
   end
-  return pytha_array
 end
 
-# 配列の３要素の総和が指定の数になるものを探す
-def sum_of_three(nat_num)
-  pythagorean(nat_num).each do |abc|
-    if abc.map{|square| Math.sqrt(square)}.sum == nat_num
-      return abc.map{|square| Math.sqrt(square)}
-    end
-  end
-end
-
-p sum_of_three(1000).inject(:*).to_i
-
+p pythagorean(1000).inject(:*).to_i
