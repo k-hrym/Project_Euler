6624b8659c2ad7534eb19817077249fbfe798377
k-saunagi <k5.daha55@gmail.com>
2020-08-26 13:04:02 +0900

    [fix]リファクタリング3.87s → 1.46s → 0.13s

--- problem9.rb
+++ problem9.rb
@@ -5,19 +5,19 @@
 # a + b + c = 1000 となるピタゴラスの三つ組が一つだけ存在する.
 # これらの積 abc を計算しなさい.
 
-# ピタゴラスの定理を満たす平方数の組み合わせ配列から求める
-def pythagorean(nat_num)
-  square_nums = [*1..nat_num].map{|int| int**2}
-  i = 0
-  square_nums.each do |num|
-    square_nums[i..-1].each do |n|
-      if square_nums.include?(num + n) && \
-        [num, n, num + n].map{|square| Math.sqrt(square)}.sum == nat_num
-        return [num, n, num + n].map{|square| Math.sqrt(square)}
+def pythagorean(sum_num)
+  [*1..sum_num].each do |num|
+    i = num + 1
+    while i <= (sum_num - num)/2
+      if Math.sqrt(num**2 + i**2)%1 == 0
+        if num + i + Math.sqrt(num**2 + i**2) == 1000
+          return [num, i, Math.sqrt(num**2 + i**2)]
+        end
       end
+      i += 1
     end
-    i += 1
   end
 end
 
 p pythagorean(1000).inject(:*).to_i
+
