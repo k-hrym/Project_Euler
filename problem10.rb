ffdc35fcea26a60eb738b752bbe6274bada50b2c
k-saunagi <k5.daha55@gmail.com>
2020-08-27 12:07:36 +0900

    [add]other solution

--- problem10.rb
+++ problem10.rb
@@ -6,3 +6,21 @@ require 'prime'
 
 p Prime.each(2000000).to_a.sum
 
+# ======組み込みライブラリ不使用=======
+# ※重たすぎる
+
+# def prime?(num)
+#   (2..num-1).each{ |x| return false if num % x == 0 }
+#   true
+# end
+
+# i = 3
+# prime_sum = 2
+# while i <= 2000000
+#   prime_sum += i if prime?(i)
+#   # 2以外の偶数は素数ではないので奇数だけ試すために+2
+#   i += 2
+# end
+
+# p prime_sum
+
