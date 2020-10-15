8efea2082b3e56c24b077b8e01039058f457ee4c
k-saunagi <k5.daha55@gmail.com>
2020-10-16 01:37:21 +0900

    finish problem31

--- problem31.rb
+++ problem31.rb
@@ -8,3 +8,24 @@
 # 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 
 # これらの硬貨を使って£2を作る方法は何通りあるか?
+
+def coin_count(coins, goal, last = 0)
+  if goal == 0
+    return 1
+  end
+
+  total = 0
+  coins.each do |c|
+    if c < last
+      next
+    end
+    if goal >= c
+      total += coin_count(coins, goal - c, c)
+    end
+  end
+
+  total
+end
+
+coins = [1,2,5,10,20,50,100,200]
+puts coin_count(coins,200)
