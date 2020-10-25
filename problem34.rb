d1d906d14b27db491f7d4bf14a714f914c48ac08
k-saunagi <k5.daha55@gmail.com>
2020-10-25 14:19:40 +0900

    finish problem34

--- problem34.rb
+++ problem34.rb
@@ -4,3 +4,26 @@
 # 各桁の数の階乗の和が自分自身と一致するような数の和を求めよ.
 
 # 注: 1! = 1 と 2! = 2 は総和に含めてはならない.
+
+def factorial(num)
+  if num == 1 || num == 0
+    return 1
+  end
+  return num * factorial(num - 1)
+end
+
+answer = 0
+3.upto(2540160) do |n|
+  sum = 0
+  n_copy = n
+  while n > 0
+    sum += factorial(n % 10)
+    n /= 10
+  end
+  if sum == n_copy
+    answer += sum
+    p sum
+  end
+end
+
+p answer
