38867e1220f6514de8ab16fac428e8ca2dd959e7
k-saunagi <k5.daha55@gmail.com>
2020-11-18 00:14:18 +0900

    finish problem40

--- problem40.rb
+++ problem40.rb
@@ -5,3 +5,21 @@
 # 小数第12位は1である.
 
 # dnで小数第n位の数を表す. d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000 を求めよ.
+
+dec = "1"
+mul_of_ten = 1
+answer_product = 1
+goal = 1000000
+2.upto(1 / 0.0) do |num|
+	if dec.size >= mul_of_ten
+		answer_product *= dec.chars[mul_of_ten - dec.size - 1].to_i
+		p dec.chars[mul_of_ten - dec.size - 1].to_i
+		mul_of_ten *= 10
+	end
+	if mul_of_ten > goal
+		break
+	end
+	dec += num.to_s
+end
+
+p answer_product
