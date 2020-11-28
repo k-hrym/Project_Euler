0506bce2a0ce0abe8039fa7e4de534ca68977365
k-saunagi <k5.daha55@gmail.com>
2020-11-28 17:58:51 +0900

    finish problem42

--- problem42.rb
+++ problem42.rb
@@ -10,3 +10,32 @@
 # 単語の値が三角数であるとき, その単語を三角語と呼ぶ.
 
 # 16Kのテキストファイル words.txt 中に約2000語の英単語が記されている. 三角語はいくつあるか?
+
+File.open('42_words.txt') do |file|
+	file.each_line do |content|
+		WORDS = content.split(',').map{|word| word.delete("\"")}
+	end
+end
+
+def triangular_num(stage)
+	(stage ** 2 + stage)/2
+end
+
+def to_int(word)
+	word.chars.map{|c| ALPHA.index(c) + 1}.sum
+end
+
+ALPHA = ("A".."Z").to_a
+to_int_words = WORDS.map{|word| to_int(word)}
+
+max_tiw = to_int_words.max
+triangular_numbers = Array.new
+
+1.upto(1/0.0) do |stage|
+	if triangular_num(stage) > max_tiw
+		break
+	end
+	triangular_numbers << triangular_num(stage)
+end
+
+p to_int_words.count{|word| triangular_numbers.include?(word)}
